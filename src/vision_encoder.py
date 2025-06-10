07ea28685e9fadee4f74ac3fe21dcc7c7ea2a343
TutMosis22 <jairo.calagua.m@uni.pe>
2025-06-10 10:56:15 -0500

    vision_encoder.py comentado

--- src/vision_encoder.py
+++ src/vision_encoder.py
@@ -10,14 +10,14 @@ class VisionEncoder(nn.Module):
     def __init__(self, device='cpu'):
         super(VisionEncoder, self).__init__()
 
-        # Cargamos un modelo ViT base preentrenado (vit_b_16 con pesos preentrenados en ImageNet)
+        # CARGAMOS UN MODELO ViT BASE PREENTRENADO (vit_b_16 con pesos preentrenados en ImageNet)
         weights = ViT_B_16_Weights.IMAGENET1K_V1
         self.model = vit_b_16(weights=weights)
 
-        # Eliminamos la capa de clasificación (head) ya que no vamos a predecir clases
+        #ELIMINAMOS LA CAPA DE CLASIFICACIÓN (HEAD) YA QUE NO VAMOS A PREDECIR CLASES
         self.model.heads = nn.Identity()
 
-        # Congelamos los parámetros (puedes descongelarlos si deseas fine-tuning)
+        #CONGELAMOS LOS PARÁMETROS (PODEMOS DESCONGELARLO SI DESEAMOS fine-tuning)
         for param in self.model.parameters():
             param.requires_grad = False
 
